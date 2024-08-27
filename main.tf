cd9ca000e1165625c0d4b12d9a8d88257f6f0fdf
vinay singh <singhvnkmr@gmail.com>
2024-08-27 15:07:17 +0530

    change in main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,28 @@ resource "aws_subnet" "public" {
     Name = "public"
   }
 }
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
